6e9fc9271ec141ef8a82ef2767f2a8da6105da83
Flo-9538 <f.mary-lachko@orange.fr>
2024-12-18 19:47:25 +0100

    Ajout Main pour lancer fenetre de connexion, modif connection

--- TCG/TradingChessGame/src/interface_/Connection.java
+++ TCG/TradingChessGame/src/interface_/Connection.java
@@ -1,45 +1,88 @@
 package interface_;
 
 import javax.swing.* ;
+
+import ex2.FenetreLitEntier.TexteListener;
+
 import java.awt.*;
 import java.awt.event.*;
-import java.util.random.RandomGeneratorFactory;
-import java.util.Random;
 
 public class Connection extends JFrame implements ActionListener{
 	
-	JButton monBouton1,monBouton2;
+	JButton seconnecter,creercompte;
+	JTextField usernamefield, passworldfield;
+	JPanel text,boutons;
 	
-	public Question() {
-		this.setTitle ("Question");
+	public Connection() {
+		this.setTitle ("Traiding Chess Game");
 		this.setSize (300, 200);
-		Random random = new Random();
 
-        // Générer un entier aléatoire dans une plage [0, max)
-        int randomInt1 = random.nextInt(1700); // entre 0 et 99
-        int randomInt2 = random.nextInt(900);
+
 		
-		this.setLocation(randomInt1, randomInt2);
-		this.setLayout(new FlowLayout());
-		monBouton1 = new JButton ("Encore ?");
-		monBouton2 = new JButton ("Stop ?");
-		monBouton1.addActionListener(this); // s’ajouter à la liste des auditeurs
-		monBouton2.addActionListener(this);
-		this.getContentPane().add(monBouton1);
-		this.getContentPane().add(monBouton2);
+		//this.setLocation(randomInt1, randomInt2);
+		this.text = new JPanel();
+		this.text.setLayout(new GridLayout(2,1));
+		this.boutons = new JPanel();
+		this.boutons.setLayout(new GridLayout(1,2));
+		
+		seconnecter = new JButton ("Se connecter");
+		creercompte = new JButton ("Creer un compte");
+		usernamefield = new JTextField ("Nom d'utilisateur");
+		passworldfield = new JTextField ("Mot de passe");
+		
+		creercompte.addActionListener(this); // s’ajouter à la liste des auditeurs
+		seconnecter.addActionListener(this);
+		usernamefield.addActionListener(this); //new TexteListener()
+		passworldfield.addActionListener(this); //new TexteListener()
+		
+		text.add(usernamefield);
+		text.add(passworldfield);
+		boutons.add(seconnecter);
+		boutons.add(creercompte);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setVisible(true);
+		this.getContentPane().add(text);
+		this.getContentPane().add(boutons);
+		this.setVisible(true); 
 	}
 	
 	public void actionPerformed(ActionEvent event) {	
-		if (event.getSource()==monBouton1) {
-			Question fen = new Question();
-		}
-		if (event.getSource()==monBouton2) {
+		if (event.getSource()==seconnecter) {
 			this.dispose();
+			//Question fen = new Question();
+			//on devra appeler la classe dao qui gère les comptes pour qu'elle nous dise si c'est le bon mdp 
+			//dans ce cas on ouvre la fenètre sur le compte de la personne	
 		}
 		
-		// bouton appelle cette méthode quand l’évènement se produit
+		if (event.getSource()==creercompte) {
+			this.dispose();
+			//on uvre la fenêtre pour creer un compte
+		}
 	} 
+	
+	/*
+	class TexteListener implements ActionListener {
+
+		public void actionPerformed(ActionEvent Event) {
+			String username= usernamefield.getText();
+			String passworld = passworldfield.getText();
+
+			try {
+				Integer.parseInt(text);
+			}
+			catch (NumberFormatException e){
+				b=false;
+			}
+
+			if (b==false) {
+				s=text+" n'est pas un entier !";
+				JOptionPane.showMessageDialog(null, s); 
+			}
+			else {
+				s=text+" est un entier !";
+				JOptionPane.showMessageDialog(null, s); 
+			}
+			
+		}
+	}*/
 }
 	
