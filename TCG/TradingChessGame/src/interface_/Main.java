c30acd1b07a2aa219a72daff9222b254f62e4e77
Tungstene74 <lb.tom37@gmail.com>
2024-12-27 18:26:39 +0100

    Pour lancer l'interface

--- TCG/TradingChessGame/src/interface_/Main.java
+++ TCG/TradingChessGame/src/interface_/Main.java
@@ -3,8 +3,7 @@ package interface_;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Connection co = new Connection(); 
+		new TCG(); 
 	}
 
 }
