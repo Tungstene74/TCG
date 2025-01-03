336fdc6704d67bf7e3a4d33b78d0aac862f0dda0
vulcannos <benji37.rob@gmail.com>
2025-01-03 16:56:48 +0100

    New sigle connection

--- TCG/TradingChessGame/src/classeDAO/SingleConnection.java
+++ TCG/TradingChessGame/src/classeDAO/SingleConnection.java
@@ -6,46 +6,45 @@ import java.sql.SQLException;
 import com.mysql.cj.jdbc.MysqlDataSource;
 
 public class SingleConnection {
-	private static Connection connect;
+		private static Connection connect;
 
-	public SingleConnection() {
-		String databaseName="sql7755320	";
-		
-		SingleConnection.connect=null;
-		
-		String url="jdbc:mysql://sql7.freesqldatabase.com :3306/"+databaseName+"?serverTimezone=UTC";
-		String login="sql7755320"; // dans l'idal un login de connexion pour l'application, et non root...
-		String password="h2IsTW68bk"; // mot de passe avec xampp
-		
-		MysqlDataSource mysqlDS = new MysqlDataSource();
-		mysqlDS.setURL(url);
-		mysqlDS.setUser(login);
-		mysqlDS.setPassword(password);
+		//Constructeur priv� pour une connexion avec MysqlDataSource
+		private SingleConnection(String serverName, String dbName, String login, String password) throws ClassNotFoundException, SQLException{
+			// Parametres de connexion : url, login, mdp
+			String url="jdbc:mysql://"+serverName+":3306/"+dbName+"?serverTimezone=UTC";
+			//port mysql avec USBWebserver:3307
+			// port mysq avec xampp 3306
 
-		try {
-			SingleConnection.connect = mysqlDS.getConnection();
-		} catch (SQLException e1) {
-			System.err.println("Erreur de parcours de connexion");
-			e1.printStackTrace();
+			// Creation d'une connexion avec MysqlDataSource
+			MysqlDataSource mysqlDS = new MysqlDataSource();
+			mysqlDS.setURL(url);
+			mysqlDS.setUser(login);
+			mysqlDS.setPassword(password);
+
+			connect = mysqlDS.getConnection();
 		}
-	}
-	
-	public static Connection getInstance() {
-		if (SingleConnection.connect == null) {
-			new SingleConnection();
+		//M�thode qui cr�e/retourne l'instance unique avec MySQLDataSource
+		public static Connection getInstance(String serverName, String dbName, String login, String password) throws ClassNotFoundException, SQLException{
+			if(connect == null){  
+				new SingleConnection(serverName, dbName, login, password);
+			}
+			return connect;   
 		}
 		
-		return SingleConnection.connect;
-		
-	}
-	
-	public static void close() {
-		try {
-			SingleConnection.connect.close();
-		} catch (SQLException e) {
-			System.err.println("Erreur de fermeture de connexion");
-			e.printStackTrace();
+		public static Connection getInstance() throws ClassNotFoundException, SQLException{
+			if(connect == null){  
+				new SingleConnection("sql7.freesqldatabase.com", "sql7755320", "sql7755320", "h2IsTW68bk");
+			}
+			return connect; 
 		}
-	}
+		
+		public static void close() {
+			try {
+				connect.close();
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
 
-}
+	}
