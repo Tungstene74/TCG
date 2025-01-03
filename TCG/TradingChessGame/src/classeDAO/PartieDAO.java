4429acc6d2957270b77d68b982b70ce1f00556d1
Flo-9538 <f.mary-lachko@orange.fr>
2025-01-03 12:08:25 +0100

    modif update de PartieDAO

--- TCG/TradingChessGame/src/classeDAO/PartieDAO.java
+++ TCG/TradingChessGame/src/classeDAO/PartieDAO.java
@@ -15,6 +15,7 @@ public class PartieDAO extends DAO<Partie>{
 	private ResultSet rs;
 
 	@Override
+	//on creer la partie en tant que joueur 1, et on creer joueur 2 à l'occasion
 	public Partie create(Partie obj) throws SQLException { //à partir d'une partie renvois une nouvelle partie avec les paramettre de la base de donné + ça l'ajoute dans la base de donné 
 		String sqlQuery = "INSERT INTO `partie`(`id_partie`, `tour_joueur`, `id_deck`, `id_joueur`,`id_deck2`, `id_joueur2`) "
 				+ "VALUES (?,?,?,?,'1','1')";
@@ -34,7 +35,8 @@ public class PartieDAO extends DAO<Partie>{
 
 		return obj;
 	}
-
+	
+	//on rejoint la partie de joueur 1 en tant que joueur 2, on récupère les info de joueur 1
 	public Partie join(int id_partie,Joueur joueur2) throws SQLException { //à partir d'id renvois une nouvelle partie avec ça version dans la base de donné 
 		String sqlQuery = "SELECT * FROM `partie` WHERE id_partie=?";
 		PreparedStatement st = connect.prepareStatement(sqlQuery);
@@ -76,6 +78,7 @@ public class PartieDAO extends DAO<Partie>{
 		return partie;
 	}
 
+	//on attend le joueur 2 en tant que joueur 1, on récupère quand il arrive les info de joueur 2 dans la bdd
 	@Override
 	public Partie update(Partie obj) throws SQLException { //à partir d'une partie renvois une nouvelle partie avec les mise à jour de la base de donné 
 		String sqlQuery = "SELECT * FROM `partie` WHERE id_partie=?";
@@ -91,13 +94,19 @@ public class PartieDAO extends DAO<Partie>{
 		int tour_joueur  = 0;
 		// Affichage du resultat
 		while(rs.next()) {
-			id_deck1  = Integer.parseInt(rs.getString("id_deck"));
-			id_joueur1  = Integer.parseInt(rs.getString("id_joueur"));
+			//id_deck1  = Integer.parseInt(rs.getString("id_deck"));
+			//id_joueur1  = Integer.parseInt(rs.getString("id_joueur"));
 			id_deck2  = Integer.parseInt(rs.getString("id_deck2"));
 			id_joueur2  = Integer.parseInt(rs.getString("id_joueur2"));
-			tour_joueur  = Integer.parseInt(rs.getString("tour_joueur"));
+			//tour_joueur  = Integer.parseInt(rs.getString("tour_joueur"));
 		}
 		
+		JoueurDAO joueurdao=new JoueurDAO();
+		Joueur joueur2= joueurdao.creerjoueur(id_joueur2);
+		Deck deck2= joueur2.mainDeck();
+		
+		obj.setdeck2(deck2);
+		obj.setjoueur2(joueur2);
 		
 		/*
 		obj.setId_deck(id_deck);
