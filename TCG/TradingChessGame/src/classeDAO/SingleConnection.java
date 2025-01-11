6fafa9251b195754fd384d75b4223827c8de4e81
vulcannos <benji37.rob@gmail.com>
2025-01-11 13:22:41 +0100

    Nouvelle base de donné.

--- TCG/TradingChessGame/src/classeDAO/SingleConnection.java
+++ TCG/TradingChessGame/src/classeDAO/SingleConnection.java
@@ -9,13 +9,13 @@ public class SingleConnection {
 	private static Connection connect;
 
 	public SingleConnection() {
-		String databaseName="sql7755320	";
+		String databaseName="if0_38085721_echec";
 		
 		SingleConnection.connect=null;
 		
-		String url="jdbc:mysql://sql7.freesqldatabase.com :3306/"+databaseName+"?serverTimezone=UTC";
-		String login="sql7755320"; // dans l'idal un login de connexion pour l'application, et non root...
-		String password="h2IsTW68bk"; // mot de passe avec xampp
+		String url="jdbc:mysql://sql308.infinityfree.com :3306/"+databaseName+"?serverTimezone=UTC";
+		String login="if0_38085721"; // dans l'idal un login de connexion pour l'application, et non root...
+		String password="fNgqgwRT21R0J3a"; // mot de passe avec xampp
 		
 		MysqlDataSource mysqlDS = new MysqlDataSource();
 		mysqlDS.setURL(url);
