e489f0aa939365c8be6ee76cb031d2caea7d37fb
vulcannos <benji37.rob@gmail.com>
2025-01-18 16:17:20 +0100

    nouvelle base de donnée

--- TCG/TradingChessGame/src/classeDAO/SingleConnection.java
+++ TCG/TradingChessGame/src/classeDAO/SingleConnection.java
@@ -13,7 +13,13 @@ public class SingleConnection {
 		
 		SingleConnection.connect=null;
 		
-		String url="jdbc:mysql://localhost :3306/"+databaseName+"?serverTimezone=UTC";
+		String mode_conection = "en ligne";
+		
+		String url="";
+		if (mode_conection== "en ligne") {
+		 url="jdbc:mysql://4.tcp.eu.ngrok.io:18562 /"+databaseName+"?serverTimezone=UTC";
+		}else { url="jdbc:mysql://2.tcp.eu.ngrok.io:15295 /"+databaseName+"?serverTimezone=UTC";
+		}
 		String login="root"; // dans l'idal un login de connexion pour l'application, et non root...
 		String password=""; // mot de passe avec xampp
 		
